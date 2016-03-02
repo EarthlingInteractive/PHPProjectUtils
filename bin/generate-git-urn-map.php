2df9319db5997c9f3cf784e85c4c19d886890bb2
Dan Stevens <stevens@earthit.com>
2016-03-02 14:32:39 -0600

    Add -i <file> option to update Git URN map files in-place.

--- bin/generate-git-urn-map.php
+++ bin/generate-git-urn-map.php
@@ -52,17 +52,90 @@ function sys($cmd) {
 	return $rez;
 }
 
+$startTime = microtime(true);
+
+$verbose = false;
+$updateFile = '-';
+for( $i=1; $i<count($argv); ++$i ) {
+	if( $argv[$i] == '-i' ) {
+		$updateFile = $argv[++$i];
+	} else if( $argv[$i] == '-v' ) {
+		$verbose = true;
+	} else if( $argv[$i] == '-?' or $argv[$i] == '-h' or $argv[$i] == '--help' ) {
+		fwrite(STDOUT, "Usage: {$argv[0]}: [-i <file>]\nPipe in a list of git object hashes.\n");
+		exit(0);
+	} else {
+		fwrite(STDERR, "{$argv[0]}: Error: Unrecognized argument: '{$argv[$i]}'\n");
+		exit(1);
+	}
+}
+
+$cachedIdMap = array(); // map of x-git-object:... => urn:sha1:...
+
+if( $updateFile ) {
+	if( file_exists($updateFile) ) {
+		$cacheStream = @fopen( $updateFile, 'rb' );
+		if( $cacheStream === false ) {
+			$err = error_get_last();
+			fwrite(STDERR, "Failed to open $updateFile for reading: ".$err['message']."\n");
+			exit(1);
+		}
+		while( ($line = fgets($cacheStream)) !== false ) {
+			$line = trim($line);
+			if( $line === '' or $line[0] === '#' ) continue;
+			$urns = explode("\t", $line);
+			$cachedIdMap[$urns[0]] = $urns[1]; // Assuming x-git-object:... urn:sha1:...
+		}
+		fclose($cacheStream);
+	}
+	// Now let's append!
+	$outstream = @fopen($updateFile, 'ab');
+	if( $outstream === false ) {
+		$err = error_get_last();
+		fwrite(STDERR, "Failed to open '$updateFile' for appending: ".$err['message']."\n");
+		exit(1);
+	}
+} else {
+	$outstream = STDOUT;
+}
+
+if( $verbose ) fwrite(STDERR, count($cachedIdMap)." entries cached\n");
+
+$calcCount = 0;
+$outputCount = 0;
+$nonBlobCount = 0;
 while( ($blobId = fgets(STDIN)) !== false ) {
 	$blobId = trim($blobId);
-	$type = sys("git cat-file -t $blobId");
-	if( $type === 'blob' ) {
+	$gitObjectUrn = "x-git-object:{$blobId}";
+	if( isset($cachedIdMap[$gitObjectUrn]) ) {
+		continue;
+	} else if( ($type = sys("git cat-file -t $blobId")) === 'blob' ) {
 		$sha1sumOutput = sys("git cat-file blob $blobId | sha1sum");
 		if( preg_match('/([0-9a-f]{40})/',$sha1sumOutput,$bif) ) {
 			$sha1Hex = $bif[1];
 			$sha1Base32 = TOGoS_Base32::encode(hex2bin($sha1Hex));
-			echo "x-git-object:{$blobId}\turn:sha1:{$sha1Base32}\n";
+			$urn = "urn:sha1:{$sha1Base32}";
 		} else {
 			fwrite(STDERR, "Bad sha1sum output: {$sha1sumOutput}\n");
+			continue;
 		}
+	} else {
+		++$nonBlobCount;
+		continue;
 	}
+	
+	fwrite($outstream, "{$gitObjectUrn}\t{$urn}\n");
+	if( ++$outputCount % 64 == 0 ) flush($outstream); // Try to flush between lines
 }
+
+if( $verbose ) {
+	$elapsedTime = microtime(true) - $startTime;
+	fwrite(
+		STDERR,
+		$outputCount." new mappings written\n".
+		$nonBlobCount." non-blobs looked at\n".
+		$elapsedTime." seconds elapsed\n"
+	);
+}
+
+if( $updateFile ) fclose($outstream);
