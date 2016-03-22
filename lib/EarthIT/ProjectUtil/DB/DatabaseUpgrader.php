883ff041874957a444bfe557808dd8bae7c2406f
Dan Stevens <stevens@earthit.com>
2016-03-22 17:09:26 -0500

    Can now load upgrade scripts from multiple directories.

--- lib/EarthIT/ProjectUtil/DB/DatabaseUpgrader.php
+++ lib/EarthIT/ProjectUtil/DB/DatabaseUpgrader.php
@@ -6,7 +6,7 @@ class EarthIT_ProjectUtil_DB_DatabaseUpgrader
 	const VERBOSITY_DUMP_SCRIPTS = 2;
 	
 	protected $sqlRunner;
-	protected $upgradeScriptDir;
+	protected $upgradeScriptDirs;
 	protected $upgradeTableSchemaName;
 	protected $upgradeTableName;
 	protected $upgradeTableExpression;
@@ -27,6 +27,13 @@ class EarthIT_ProjectUtil_DB_DatabaseUpgrader
 	 * SQLRunners, but that would be a major change.
 	 */
 	
+	/*
+	 * When upgrade scripts are represented as an array, it looks like this: {
+	 *   "scriptfilename": basename of script; used for determining order
+	 *   "scriptfilehash": hex-encoded SHA-1 of script
+	 *   "scriptfilepath": path to script file
+	 */
+	
 	public $allowOutOfOrderScripts;
 	public $verbosity = 0;
 	public $actuallyDoUpgrades = true;
@@ -38,9 +45,10 @@ class EarthIT_ProjectUtil_DB_DatabaseUpgrader
 	 */
 	public $dumpUpgradesToStdout = false;
 	
-	public function __construct( $sqlRunner, $upgradeScriptDir ) {
+	public function __construct( $sqlRunner, $upgradeScriptDirs ) {
+		if( !is_array($upgradeScriptDirs) ) $upgradeScriptDirs = array($upgradeScriptDirs);
 		$this->sqlRunner = $sqlRunner;
-		$this->upgradeScriptDir = $upgradeScriptDir;
+		$this->upgradeScriptDirs = $upgradeScriptDirs;
 		$this->setUpgradeTable('schemaupgrade');
 	}
 	
@@ -205,9 +213,8 @@ class EarthIT_ProjectUtil_DB_DatabaseUpgrader
 	/* Here endeth the list of functions that are expected to be
 	 * overridden. */
 	
-	protected function runPhpScript($text, $name) {
-		require "{$this->upgradeScriptDir}/{$name}";
-		// eval("? >".$text."< ? php");
+	protected function runPhpScript($text, $phpFile) {
+		require $phpFile;
 	}
 	
 	/**
@@ -216,7 +223,7 @@ class EarthIT_ProjectUtil_DB_DatabaseUpgrader
 	 * @param string $hash hex-encoded SHA-1 of SQL
 	 * @param boolean $useTransaction whether or not the upgrade should be wrapped in a transaction
 	 */
-	protected function doUpgrade($usText, $usName, $hash, $useTransaction) {
+	protected function doUpgrade(array $upgradeScript, $useTransaction) {
 		// Need to use exec specifically (rather than query or fetchAll)
 		// to avoid attempting to 'prepare' the statement, as PDO does not allow
 		// multiple commands in one prepared statement.
@@ -224,24 +231,96 @@ class EarthIT_ProjectUtil_DB_DatabaseUpgrader
 		// run scripts twice.
 		// TODO: Actually do into the above TODO sometimes; it's known to be a real problem
 		// on sites with multiple deployments trying to upgrade the same database.
+
+		$usText = $upgradeScript['scriptfilecontent'];
+		$usName = $upgradeScript['scriptfilename'];
+		$usPath = $upgradeScript['scriptfilepath'];
+		$usHash = $upgradeScript['scriptfilehash'];
+		
 		if( $useTransaction ) $this->beginTransaction();
 		try {
 			if( preg_match('/\.(php|sql)$/',$usName,$bif) ) {
 				switch( $ext = $bif[1] ) {
 				case 'sql': $this->doRawQuery($usText); break;
-				case 'php': $this->runPhpScript($usText, $usName); break;
+				case 'php': $this->runPhpScript($usText, $usPath); break;
 				default: throw new Exception("'$ext'?", "{$this->upgradeScriptDir}/{$usName}");
 				}
 			} else {
 				throw new Exception("Can't glean upgrade script type from name: '$usName'");
 			}
-			$this->logUpgrade($usName, $hash);
+			$this->logUpgrade($usName, $usHash);
 			if( $useTransaction ) $this->commitTransaction();
 		} catch( Exception $e ) {
 			if( $useTransaction ) $this->cancelTransaction();
 			fputs(STDERR, "Error while running '$usName' : ".$e->getMessage()."\n");
 			throw new Exception("Error while running '$usName'", 0, $e);
 		}
+	}
+	
+	protected function _findUpgradeScripts($dir, array &$upgradeScripts, array &$errors) {
+		$dh = opendir($dir);
+		if( $dh === false ) {
+			throw new Exception("Failed to open upgrade script directory '$dir'");
+		}
+		
+		$junkFiles = array();
+		while( ($fn = readdir($dh)) !== false ) {
+			if( preg_match('/^\./',$fn) ) continue;
+			$fullpath = "{$dir}/{$fn}";
+			if( is_dir($fullpath) ) {
+				$this->_findUpgradeScripts($fullpath, $upgradeScripts, $junkFiles);
+			} else if( preg_match('/\.(?:sql|php)$/',$fn) ) {
+				if( isset($upgradeScripts[$fn]) ) {
+					$oldFullPath = $upgradeScripts[$fn]['scriptfilepath'];
+					$errors['duplicateScripts'][$fn][$oldFullPath] = $upgradeScripts[$fn];
+					$errors['duplicateScripts'][$fn][$fullpath] = $upgradeScript;
+				}
+				
+				$upgradeScripts[$fn] = array(
+					'scriptfilename' => $fn,
+					'scriptfilepath' => $fullpath,
+				);
+			} else {
+				$errors['junkFiles'][$fullpath] = $fullpath;
+				continue;
+			}
+		}
+		closedir($dh);
+		
+		return count($errors) == 0; // Not quite the right logic but will work for now
+	}
+	
+	public function findUpgradeScripts() {
+		$upgradeScripts = array();
+		$errors         = array();
+		
+		$success = true;
+		foreach( $this->upgradeScriptDirs as $dir ) {
+			$success &= $this->_findUpgradeScripts($dir, $upgradeScripts, $errors);
+		}
+		
+		$anyErrors = false;
+		if( !empty($errors['junkFiles']) ) {
+			fwrite(STDERR, "Error: There is extra junk in your upgrade scripts directory:\n");
+			foreach( $junkFiles as $f ) {
+				fwrite(STDERR, "  $f\n");
+			}
+			$anyErrors = true;
+		}
+		if( !empty($errors['duplicateUpgradeScripts']) ) {
+			fwrite(STDERR, "Error: Some scripts are duplicated (same name in different directories):\n");
+			foreach( $duplicateUpgradeScripts as $us=>$versions ) {
+				fwrite(STDERR, "  $us: ".implode(', ',array_keys($versions))."\n");
+			}
+			$anyErrors = true;
+		}
+		if( !$success ) {
+			fwrite(STDERR, "Aborting.  Clean up the upgrade scripts directory and try again.\n");
+			exit(1);
+		}
+		
+		ksort($upgradeScripts);
+		return $upgradeScripts;
 	}
 	
 	/**
@@ -255,49 +334,22 @@ class EarthIT_ProjectUtil_DB_DatabaseUpgrader
 		$this->shouldDoQueries = true;
 		$this->shouldDumpQueries = false;
 		
-		$upgradesAlreadyRun = $this->getUpgradesAlreadyRun();
-		
-		$upgradeScripts = array();
-		$dh = opendir($this->upgradeScriptDir);
-		if( $dh === false ) {
-			throw new Exception("Failed to open upgrade script directory");
-		}
-		$junkFiles = array();
-		while( ($fn = readdir($dh)) !== false ) {
-			if( preg_match('/^\./',$fn) ) continue;
-			if( !preg_match('/\.(?:sql|php)$/',$fn) ) {
-				$junkFiles[] = $fn;
-				continue;
-			}
-	
-			$upgradeScripts[] = $fn;
-		}
-		closedir($dh);
-		
-		if( count($junkFiles) > 0 ) {
-			fwrite(STDERR, "Error: There is extra junk in your upgrade scripts directory:\n");
-			foreach( $junkFiles as $f ) {
-				fwrite(STDERR, "  $f\n");
-			}
-			fwrite(STDERR, "Aborting.  Clean up the upgrade scripts directory and try again.\n");
-			exit(1);
-		}
-		
-		sort($upgradeScripts);
-		
+		$upgradeScripts = $this->findUpgradeScripts(); // scriptfilename => array(...)
+		$upgradesAlreadyRun = $this->getUpgradesAlreadyRun(); // scriptfilename => array(...)
 		$alreadyRunOutOfOrderScripts = array();
 		$upgradeScriptsToRun = array();
-		foreach( $upgradeScripts as $us ) {
+		
+		foreach( $upgradeScripts as $us=>$upgradeScript ) {
 			if( !isset($upgradesAlreadyRun[$us]) ) {
-				$upgradeScriptsToRun[] = $us;
+				$upgradeScriptsToRun[$us] = $upgradeScript;
 			} else if( count($upgradeScriptsToRun) > 0 ) {
-				$alreadyRunOutOfOrderScripts[] = $us;
+				$alreadyRunOutOfOrderScripts[$us] = $upgradeScript;
 			}
 		}
 		
 		if( !$this->allowOutOfOrderScripts and count($alreadyRunOutOfOrderScripts) > 0 ) {
 			fwrite(STDERR, "Error: Some scripts after the first unrun one ({$upgradeScriptsToRun[0]}) have already been run:\n");
-			foreach( $alreadyRunOutOfOrderScripts as $s ) {
+			foreach( $alreadyRunOutOfOrderScripts as $s=>$_ ) {
 				fwrite(STDERR, "  $s\n");
 			}
 			fwrite(STDERR, "Aborting.  Pass -f to force upgrades to run anyway.\n");
@@ -308,8 +360,8 @@ class EarthIT_ProjectUtil_DB_DatabaseUpgrader
 			fwrite(STDOUT, "-- ".count($upgradeScriptsToRun)." scripts to be run:\n");
 			if( $upgradeScriptsToRun ) {
 				fwrite(STDOUT,
-					"-- From ".$upgradeScriptsToRun[0]." to ".
-					$upgradeScriptsToRun[count($upgradeScriptsToRun)-1]."\n"
+					"-- From ".self::firstKey($upgradeScriptsToRun)." to ".
+					self::lastKey($upgradeScriptsToRun)."\n"
 				);
 			}
 		}
@@ -317,8 +369,8 @@ class EarthIT_ProjectUtil_DB_DatabaseUpgrader
 		$this->shouldDoQueries = $this->actuallyDoUpgrades;
 		$this->shouldDumpQueries = $this->dumpUpgradesToStdout;
 		
-		foreach( $upgradeScriptsToRun as $us ) {
-			$usFile = "{$this->upgradeScriptDir}/$us";
+		foreach( $upgradeScriptsToRun as $us=>$upgradeScript ) {
+			$usFile = $upgradeScript['scriptfilepath'];
 			$usText = file_get_contents($usFile);
 			
 			$useTransaction = true;
@@ -349,7 +401,10 @@ class EarthIT_ProjectUtil_DB_DatabaseUpgrader
 				echo "\n-- $us\n";
 			}
 			
-			$this->doUpgrade($usText, $us, $hash, $useTransaction);
+			$upgradeScript['scriptfilehash'] = $hash;
+			$upgradeScript['scriptfilecontent'] = $usText;
+			
+			$this->doUpgrade($upgradeScript, $useTransaction);
 			
 			if( $updatesUpgradeLog ) {
 				if( $this->verbosity >= self::VERBOSITY_LIST_SCRIPTS ) {
